1707a61443dc2c073f053014e0ee87e0fd333a37
Leandro Burgos <burgosleandro.c@gmail.com>
2023-10-07 20:00:49 -0300

    add database url and method database connection

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  findAll(): string {
+    return 'Hola, mundo! Desde el backend de ShopFleet';
+  }
 
   @Get()
   getHello(): string {
---
+++ backend/src/database/database.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
